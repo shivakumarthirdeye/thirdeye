96d432cf0bffc78b5834ffdf5dfeeff4a93d4e4b
Yogesh Kumar <77193839+Yogesh011-R@users.noreply.github.com>
2022-11-28 12:26:36 +0530

    countup

--- components/common/ImpactCreated.js
+++ components/common/ImpactCreated.js
@@ -1,13 +1,19 @@
 import { GreenRight } from './ShadeSVGs';
+import CountUp, { useCountUp } from 'react-countup';
 
 const impacts = [
-  { id: 1, amount: 10, description: 'Years of experience' },
-  { id: 2, amount: 200, description: 'Projects completed' },
-  { id: 3, amount: 90, description: 'Clients Served' },
-  { id: 4, amount: '220m', description: 'Lives Impacted' },
+  { id: 1, ending: '+', amount: 10, description: 'Years of experience' },
+  { id: 2, ending: '+', amount: 200, description: 'Projects completed' },
+  { id: 3, ending: '+', amount: 90, description: 'Clients Served' },
+  { id: 4, ending: 'M+', amount: 220, description: 'Lives Impacted' },
 ];
 
 const ImpactCreated = () => {
+  useCountUp({
+    ref: 'counter',
+    enableScrollSpy: true,
+    scrollSpyDelay: 1000,
+  });
   return (
     <div className='relative '>
       <div className='container mx-auto px-4 overflow-hidden'>
@@ -15,11 +21,15 @@ const ImpactCreated = () => {
 
         <div className='grid grid-cols-2 gap-y-5 lg:grid-cols-4 my-10 lg:my-20'>
           {impacts.map(impact => {
-            const { id, amount, description } = impact;
+            const { id, amount, ending, description } = impact;
 
             return (
               <div key={id}>
-                <h1 className='text-3xl md:text-4xl lg:text-5xl'>{amount}+</h1>
+                <h1 className='text-3xl md:text-4xl lg:text-5xl'>
+                  <CountUp end={amount} start={0} enableScrollSpy />
+                  {ending}
+                  {/* {amount}+ */}
+                </h1>
                 <h3 className='text-black text-opacity-80  text-sm sm:text-base md:text-xl lg:text-2xl my-2'>
                   {description}
                 </h3>
