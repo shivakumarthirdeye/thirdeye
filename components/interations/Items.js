85b2eb293b2f30f329559ef34c4f0fb2c81f5ccc
Yogesh <yogesh.kumar.rr5@gmail.com>
2023-01-16 19:26:43 +0530

    fixes

--- components/interations/Items.js
+++ components/interations/Items.js
@@ -8,19 +8,24 @@ const Items = ({ title, description, id, sub, video }) => {
   const playerRef = useRef(null);
   useEffect(() => {}, []);
   return (
-    <div className='grid grid-cols-2 gap-10 items-center'>
-      <div className={`${id % 2 === 0 ? 'order-1' : 'order-2'} flex-1`}>
-        <h1 className='text-6xl mb-4'>
+    <div className='grid lg:grid-cols-2 gap-10  items-center px-4'>
+      <div
+        className={`${id % 2 === 0 ? 'lg:order-1' : 'lg:order-2'} mt-10 lg:mt-0
+         flex-1`}
+      >
+        <h1 className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl mb-4'>
           {title}
-          <span className='text-4xl'>{sub}</span>
+          <span className='text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
+            {sub}
+          </span>
         </h1>
-        <p className='text-2xl text-black text-opacity-60 !leading-[35.4px]'>
+        <p className='sm:text-lg md:text-xl lg:text-2xl text-black text-opacity-60 !leading-[35.4px]'>
           {description}
         </p>
       </div>
       <div
         className={`${
-          id % 2 === 0 ? 'order-2 ' : 'order-1 '
+          id % 2 === 0 ? 'lg:order-2 ' : 'lg:order-1 '
         }  items-center mx-auto justify-center flex-1`}
       >
         <video src={video} autoPlay loop muted></video>
